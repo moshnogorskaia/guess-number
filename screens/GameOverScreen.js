4d2ebc8c768ea4a2ee87451c12f2ed830de1a21e
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-12 10:15:49 +0100

    Add custom fonts

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,12 @@
-import { Text } from "react-native";
+import { View } from "react-native";
+import Title from "../components/ui/Title";
 
 function GameOverScreen({ roundsNumber, userNumber, onRestart }) {
-	return <Text>Game Over!</Text>;
+	return (
+		<View>
+			<Title></Title>
+		</View>
+	);
 }
 
 export default GameOverScreen;
