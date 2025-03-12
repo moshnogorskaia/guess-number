83688a5f48e7c536a211ad709a8a8d121c6f6423
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-12 09:59:23 +0100

    Add icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
-import { View, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert, Text } from "react-native";
 import Title from "../components/ui/Title";
 import { useState, useEffect } from "react";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import { Ionicons } from "@expo/vector-icons";
 
 function generateRandomBetween(min, max, exclude) {
 	const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -55,17 +56,25 @@ function GameScreen({ userNumber, onGameOver }) {
 			<Title>Opponents Guess</Title>
 			<NumberContainer>{currentGuess}</NumberContainer>
 			<Card>
-				<InstructionText>Higher or lower?</InstructionText>
-				<View>
-					<PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-						-
-					</PrimaryButton>
-					<PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-						+
-					</PrimaryButton>
+				<InstructionText style={styles.instructionText}>
+					Higher or lower?
+				</InstructionText>
+				<View style={styles.buttonsContainer}>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+							<Ionicons name="remove" size={24} color="white" />
+						</PrimaryButton>
+					</View>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+							<Ionicons name="add" size={24} color="white" />
+						</PrimaryButton>
+					</View>
 				</View>
 			</Card>
-			<View>Log rounds</View>
+			<View>
+				<Text>Log rounds</Text>
+			</View>
 		</View>
 	);
 }
@@ -77,13 +86,13 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 24,
 	},
-	title: {
-		fontSize: 24,
-		fontWeight: "bold",
-		color: "#ddb52f",
-		textAlign: "center",
-		borderWidth: 2,
-		borderColor: "#ddb52f",
-		padding: 12,
+	buttonsContainer: {
+		flexDirection: "row",
+	},
+	buttonContainer: {
+		flex: 1,
+	},
+	instructionText: {
+		marginBottom: 12,
 	},
 });
